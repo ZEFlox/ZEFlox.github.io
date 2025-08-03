324c2fa0bff3f984e9b83cf39ef29fa8ad19a795
ZEFlox <zeflox@163.com>
2025-08-03 22:51:45 +0800

    littlefix

--- kardsdeckbuilder/components/Deck.vue.js
+++ kardsdeckbuilder/components/Deck.vue.js
@@ -58,6 +58,7 @@ debugger; /* PartiallyEnd: #3632/scriptSetup.vue */
 const __VLS_ctx = {};
 let __VLS_components;
 let __VLS_directives;
+/** @type {__VLS_StyleScopedClasses['count']} */ ;
 // CSS variable injection 
 // CSS variable injection end 
 __VLS_asFunctionalElement(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)(Object.assign({ class: "deck" }));
